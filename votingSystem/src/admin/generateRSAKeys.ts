e95972cd5fbea628a0657612822b794c0844c3cf
j0xn <17837186+shadmau@users.noreply.github.com>
2024-05-03 09:54:51 +0000

    Add RSA key pair generation functions for Register and Election Coordinator

---
+++ votingSystem/src/admin/generateRSAKeys.ts
@@ -0,0 +1,43 @@
+import { generateKeyPairSync } from 'crypto';
+import NodeRSA from 'node-rsa';
+
+/**
+ * Generates an RSA public-private key pair.
+ * @returns An object containing the publicKey and privateKey.
+ */
+export function generateKeyPair(): { publicKey: string, privateKey: string } {
+    const { publicKey, privateKey } = generateKeyPairSync('rsa', {
+        modulusLength: 2048,
+        publicKeyEncoding: {
+            type: 'spki',
+            format: 'der'
+        },
+        privateKeyEncoding: {
+            type: 'pkcs8',
+            format: 'der'
+        }
+    });
+
+    return {
+        publicKey: '0x' + publicKey.toString('hex'),
+        privateKey: '0x' + privateKey.toString('hex')
+    };
+}
+
+
+/**
+ * Generates a raw RSA public-private key pair for blind signatures.
+ * @returns An object containing the raw components of the RSA keys in hex.
+ */
+export function generateKeyPairRaw(): { e: string, n: string, d:string }{
+    const key = new NodeRSA({ b: 2048 });
+
+    const publicKeyComponents = key.exportKey('components-public');
+    const privateKeyComponents = key.exportKey('components-private');
+
+    return {
+        e: '0x' + (Buffer.isBuffer(publicKeyComponents.e) ? publicKeyComponents.e.toString('hex') : publicKeyComponents.e.toString(16)),
+        n: '0x' + publicKeyComponents.n.toString('hex'), 
+        d: '0x' + privateKeyComponents.d.toString('hex') 
+     };
+}
