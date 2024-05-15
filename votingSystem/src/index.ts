9d5bfd9b061db866824f6fead771060649dadcc2
j0xn <17837186+shadmau@users.noreply.github.com>
2024-05-15 04:55:04 +0000

    Add exposing signVotingTransaction, generateRSAKeys, and voting functions

--- votingSystem/src/index.ts
+++ votingSystem/src/index.ts
@@ -3,3 +3,6 @@ export * from './utils/utils';
 export * from './types/types';
 export * from './blind-signature/generateTokens';
 export * from './voter-credentials/voterCredentials';
+export * from './admin/signVotingTransaction';
+export * from './admin/generateRSAKeys';
+export * from './voting/voting';
