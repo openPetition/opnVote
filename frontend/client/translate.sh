6857283b579c9621fb49cc3cca9419d279b56a23
svalyi-op <129736832+svalyi-op@users.noreply.github.com>
2024-09-09 16:28:17 +0200

    change script for macOS

--- frontend/client/translate.sh
+++ frontend/client/translate.sh
@@ -17,12 +17,12 @@ fi
 echo 'copy the following (without the start and end delimiter) to deepl / whatever:'
 echo '##### start #####'
 
-cat $1 | jq . | head -n -1  | tail -n +2 | sed -e 's/^ *"\([^"]\+\)": "\(.*\)",\?$/\1 ### \2/g'
+cat $1 | jq . | sed 1d  | sed '$d' | sed -E 's/^ *"([^"]+)": "(.*)",?$/\1 ### \2/g'
 
 echo '##### end #####'
 
 echo "when deepl / whatever is done, copy its output into the shell. when you're done, press enter for an empty line"
-truncate --size 0 translate.tmp
+truncate -s 0 translate.tmp
 while IFS= read -r line; do
     if [ "$line" == "" ]; then
         break
@@ -31,7 +31,7 @@ while IFS= read -r line; do
 done
 
 echo '{' > translate.out
-cat translate.tmp | sed -e  's/^ *\([^ ]\+\) *### *\(.*\)$/"\1": "\2",/' >> translate.out
+cat translate.tmp | sed -E  's/^ *([^ ]+) *### *(.*)$/"\1": "\2",/' >> translate.out
 echo ' "":""}' >> translate.out
 
 cat translate.out | jq -r 'del(.[] | select(. == ""))' > $2
@@ -41,7 +41,10 @@ rm translate.out
 
 echo -n 'checking for all keys to be present ...'
 
-if [ `jq -c ". | keys" $1` != `jq -c ". | keys" $2` ]; then
+original=`jq -c ". | keys" $1`
+new=`jq -c ". | keys" $2`
+
+if [ "$original" != "$new" ]; then
     echo ' files seem to have different keys'
     echo 'ERROR!!!'
 else
