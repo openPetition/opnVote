745b30aca31e92534024516cba432b189175e28a
j0xn <17837186+shadmau@users.noreply.github.com>
2025-02-24 06:23:55 +0100

    feat(sc, vs): Add deployment script for v0.0.2; remove unnecessary test script

---
+++ backend/smart-contracts/0.0.2/script/Deploy.s.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {Script, console} from "forge-std/Script.sol";
+import {OpnVote} from "../src/OpnVote.sol";
+
+contract DeployScript is Script {
+    function setUp() public {}
+
+    function run() public {
+        uint256 deployerPrivkey = vm.envUint("DEPLOYER_PRIV_KEY");
+        address trustedForwarder = vm.envAddress("GELATO_TRUSTED_FORWARDER");
+
+        vm.startBroadcast(deployerPrivkey);
+        OpnVote opnVote = new OpnVote(trustedForwarder);
+        console.log(address(opnVote));
+    }
+}
