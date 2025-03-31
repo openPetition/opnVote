d56b15399f86b4bb401cae242436c95d6ab46c8f
svalyi-op <129736832+svalyi-op@users.noreply.github.com>
2025-03-31 14:35:50 +0200

    Imp: footer style and wordings (L101)
    L101

--- frontend/client/src/components/Footer.jsx
+++ frontend/client/src/components/Footer.jsx
@@ -6,73 +6,53 @@ import NextImage from 'next/image';
 import { useTranslation } from "next-i18next";
 
 export default function Footer() {
-	const { t } = useTranslation();
+    const { t } = useTranslation();
 
     return (
         <>
-
-			<footer>
-				<FooterDonationBox />
-					<div className={styles.greyblock}>
-						<div className={styles.greyblockcontent}>
-							<div className={styles.checkmarks}>
-								<span className={styles.checkmark}>{t('footer.checkmarks.secret')}</span>
-								<span className={styles.checkmark}>{t('footer.checkmarks.verifiable')}</span>
-								<span className={styles.checkmark}>{t('footer.checkmarks.decentralized')}</span>
-							</div>
-							<div className={styles.logo}>
-									{t('footer.logo.text')}
-									<NextImage
-										alt="openpetition logo"
-										src="/images/openpetition-logo.png"
-										height={68}
-										width={194}
-										style={{ marginTop: ".5rem" }}
-									/>
-							</div>
-						</div>
-					</div>
-					<div className={styles.textblock}>
-						<div className={styles.textblockcontent}>
-							<div className={styles.copyright}>
-								<a href="https://creativecommons.org" rel="external nofollow">
-									<NextImage
-										src="/images/creative-commons.svg"
-										height={20}
-										width={20}
-										alt="creative commons"
-									/>
-								</a>
-								{new Date().getFullYear()}
-								<a href="https://openpetition.net/" className={styles.link}>openPetition gGmbH</a>
-							</div>
-
-							<div className={styles.links}>
-								<a href="#" className={styles.link}>{t('footer.links.dataprivacy')} | </a>
-								<a href="#" className={styles.link}>{t('footer.links.impressum')} | </a>
-								<a href="#" className={styles.link}>{t('footer.links.transparency')}</a>
-							</div>
-
-							<div className={styles.icons}>
-								<a href="#" className={styles.icon}>
-									<svg width="20px" height="20px" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="white" viewBox="0 0 6 17">
-										<path fillRule="evenodd" d="M6.135 3H8V0H6.135a4.147 4.147 0 0 0-4.142 4.142V6H0v3h2v9.938h3V9h2.021l.592-3H5V3.591A.6.6 0 0 1 5.592 3h.543Z" clipRule="evenodd" />
-									</svg>
-									<span className="sr-only">Facebook Seite</span>
-								</a>
-								<a href="#" className={styles.icon}>
-									<svg width="20px" height="20px" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="white" viewBox="0 0 1350 1100">
-										<path fillRule="evenodd" d="M714.163 519.284L1160.89 0H1055.03L667.137 450.887L357.328 0H0L468.492 681.821L0 1226.37H105.866L515.491 750.218L842.672 1226.37H1200L714.137 519.284H714.163ZM569.165 687.828L521.697 619.934L144.011 79.6944H306.615L611.412 515.685L658.88 583.579L1055.08 1150.3H892.476L569.165 687.854V687.828Z" clipRule="evenodd" />
-									</svg>
-
-									<span className="sr-only">Twitter Seite</span>
-								</a>
-							</div>
-						</div>
-					</div>
-
+            <footer>
+                <FooterDonationBox />
+                <div className={styles.greyblock}>
+                    <div className={styles.greyblockcontent}>
+                        <div className={styles.checkmarks}>
+                            <span className={styles.checkmark}>{t('footer.checkmarks.secret')}</span>
+                            <span className={styles.checkmark}>{t('footer.checkmarks.verifiable')}</span>
+                            <span className={styles.checkmark}>{t('footer.checkmarks.decentralized')}</span>
+                        </div>
+                        <div className={styles.logo}>
+                            {t('footer.logo.text')}
+                            <NextImage
+                                alt="openpetition logo"
+                                src="/images/openpetition-logo.png"
+                                height={68}
+                                width={194}
+                                style={{ marginTop: ".5rem" }}
+                            />
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.textblock}>
+                    <div className={styles.textblockcontent}>
+                        <div className={styles.copyright}>
+                            <a href="https://creativecommons.org" rel="external nofollow">
+                                <NextImage
+                                    src="/images/creative-commons.svg"
+                                    height={20}
+                                    width={20}
+                                    alt="creative commons"
+                                />
+                            </a>
+                            {new Date().getFullYear()}
+                            <a href="https://openpetition.net/" className={styles.link}>openPetition gGmbH</a>
+                        </div>
+                        <div className={styles.links}>
+                            <a href="https://www.openpetition.de/content/data_privacy" className={styles.link}>{t('footer.links.dataprivacy')}</a>
+                            <a href="https://www.openpetition.de/content/legal_details" className={styles.link}>{t('footer.links.impressum')}</a>
+                            <a href="https://www.openpetition.de/blog/transparenzinitiative" className={styles.link}>{t('footer.links.transparency')}</a>
+                        </div>
+                    </div>
+                </div>
             </footer>
-
         </>
     );
 }
--- frontend/client/src/components/FooterDonationBox.jsx
+++ frontend/client/src/components/FooterDonationBox.jsx
@@ -2,23 +2,30 @@
 
 import styles from '../styles/FooterDonationBox.module.css';
 import Button from './Button';
+import { Heart } from 'lucide-react';
+import { useTranslation } from "next-i18next";
 
 export default function FooterDonationBox() {
-	return (
-		<>
-			<div className={styles.box}>
-				<div className={styles.blueheartbox}>
-					<p className={styles.textblock}>
-						Helfen Sie uns demokratische Online-Wahlen möglich zu machen. Um die Plattform kostenlos bereitzustellen, sind wir auf Spenden angewiesen.
-					</p>
-					<Button
-						onClickAction={() => { window.location = 'https://openpetition.org/spenden'; }}
-						type="dark"
-						text="JETZT SPENDEN"
-						style={{ display: 'block', margin: '0 auto', marginTop: '1.5rem', fontWeight: 'bold', padding: '10px 15px' }}
-					/>
-				</div>
-			</div>
-		</>
-	);
+    const { t } = useTranslation();
+
+    return (
+        <>
+            <div className={styles.box}>
+                <div className={styles.blueheartbox}>
+                    <div className={styles.heart}>
+                        <Heart fill="#fff" size={32} />
+                    </div>
+                    <p className={styles.textblock}>
+                        {t('footer.donationbox.donationtext')}
+                    </p>
+                    <Button
+                        onClickAction={() => { window.location = 'https://openpetition.org/spenden'; }}
+                        type="dark"
+                        text={t('footer.donationbox.donatenow')}
+                        style={{ display: 'block', margin: '0 auto', marginTop: '1.5rem', fontWeight: 'bold', padding: '10px 15px', textTransform: 'uppercase' }}
+                    />
+                </div>
+            </div>
+        </>
+    );
 }
