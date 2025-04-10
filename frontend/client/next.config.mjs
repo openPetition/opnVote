bb93c5221cf5daf3d3defa57c92bfa07843c0832
jtopenpetition <70581149+jtopenpetition@users.noreply.github.com>
2025-04-10 10:43:13 +0200

    add another config for actual prod, and staging

--- frontend/client/next.config.mjs
+++ frontend/client/next.config.mjs
@@ -13,9 +13,9 @@ const devConfig = {
     images: {
         domains: ['localhost', 'static.openpetition.de', 'client-test.opn.vote'],
     },
-}
+};
 
-const prodConfig = {
+const stagingConfig = {
     i18n,
     env: {
         basicUrl: 'https://client-test.opn.vote',
@@ -42,8 +42,37 @@ const prodConfig = {
         unoptimized: true,
         domains: ['localhost', 'static.openpetition.de'],
     },
-}
+};
+
+const prodConfig = {
+    i18n,
+    env: {
+        basicUrl: 'https://client.opn.vote',
+        abiConfigUrl: 'https://client.opn.vote/api/abi.json',
+        blindedSignatureUrl: 'https://register.opn.vote/api/sign',
+        signVotingTransactionUrl: 'https://svs.opn.vote/api/votingTransaction/sign',
+        graphConnectUrl: 'https://graphql.opn.vote/subgraphs/name/opnvote-002',
+        gelatoForwardUrl: 'https://svs.opn.vote/api/gelato/forward',
+        maxVoteRecasts: 2,
+    },
+
+    output: 'export',
+    // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+    trailingSlash: true,
+
+    //Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+    skipTrailingSlashRedirect: false,
+    assetPrefix: "https://client.opn.vote/",
+
+    // Optional: Change the output directory `out` -> `dist`
+    distDir: 'dist',
+    reactStrictMode: true,
+    images: {
+        unoptimized: true,
+        domains: ['localhost', 'static.openpetition.de'],
+    },
+};
 
 /** @type {import('next').NextConfig} **/
-const nextConfig = process.env.NODE_ENV === 'production' ? prodConfig : devConfig;
+const nextConfig = process.env.NODE_ENV === 'production' ? prodConfig : (process.env.NODE_ENV === 'staging' ? stagingConfig : devConfig);
 export default nextConfig;
