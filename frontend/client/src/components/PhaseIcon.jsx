f01a10b65585864f707d611cc19064b41fb8ab5f
jtopenpetition <70581149+jtopenpetition@users.noreply.github.com>
2025-07-02 15:25:56 +0200

    fix: fix PhaseIcon svg attributes so dumb react can understand

--- frontend/client/src/components/PhaseIcon.jsx
+++ frontend/client/src/components/PhaseIcon.jsx
@@ -34,32 +34,32 @@ export default function PhaseIcon({type, variant="blue", width=50, height=50, cl
             {circleFill && (<circle stroke="none" fill={circleFill} cy="25" cx="25" r="23" />)}
             {type == "id" && (
                 <g>
-                    <path d="m 29,23 h 2" fill="none" stroke={color} stroke-linecap="round" stroke-linejoin="round" stroke-width="2" />
-                    <path d="m 29,27 h 2" fill="none" stroke={color} stroke-linecap="round" stroke-linejoin="round" stroke-width="2" />
-                    <path d="m 19.17,28 a 3,3 0 0 1 5.66,0" fill="none" stroke={color} stroke-linecap="round" stroke-linejoin="round" stroke-width="2" />
-                    <circle cx="22" cy="24" r="2" fill="none" stroke={color} stroke-linecap="round" stroke-linejoin="round" stroke-width="2" />
-                    <rect width="20" height="14" rx="2" fill="none" stroke={color} stroke-linecap="round" stroke-linejoin="round" stroke-width="2" x="15" y="18" />
+                    <path d="m 29,23 h 2" fill="none" stroke={color} strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" />
+                    <path d="m 29,27 h 2" fill="none" stroke={color} strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" />
+                    <path d="m 19.17,28 a 3,3 0 0 1 5.66,0" fill="none" stroke={color} strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" />
+                    <circle cx="22" cy="24" r="2" fill="none" stroke={color} strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" />
+                    <rect width="20" height="14" rx="2" fill="none" stroke={color} strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" x="15" y="18" />
                 </g>
             )}
             {type == "ballot" && (
                 <g id="receipt-text" transform="translate(13 13)">
-                    <path d="M4,2V22l2-1,2,1,2-1,2,1,2-1,2,1,2-1,2,1V2L18,3,16,2,14,3,12,2,10,3,8,2,6,3Z" fill="none" stroke={color} stroke-linecap="round" stroke-linejoin="round" stroke-width="2"/>
-                    <path d="M14,8H8" fill="none" stroke={color} stroke-linecap="round" stroke-linejoin="round" stroke-width="2"/>
-                    <path d="M16,12H8" fill="none" stroke={color} stroke-linecap="round" stroke-linejoin="round" stroke-width="2"/>
-                    <path d="M13,16H8" fill="none" stroke={color} stroke-linecap="round" stroke-linejoin="round" stroke-width="2"/>
+                    <path d="M4,2V22l2-1,2,1,2-1,2,1,2-1,2,1,2-1,2,1V2L18,3,16,2,14,3,12,2,10,3,8,2,6,3Z" fill="none" stroke={color} strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"/>
+                    <path d="M14,8H8" fill="none" stroke={color} strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"/>
+                    <path d="M16,12H8" fill="none" stroke={color} strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"/>
+                    <path d="M13,16H8" fill="none" stroke={color} strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"/>
                 </g>
             )}
             {type == "key" && (
                 <g transform="translate(13 13)">
-                    <path d="M2.586,17.414A2,2,0,0,0,2,18.828V21a1,1,0,0,0,1,1H6a1,1,0,0,0,1-1V20a1,1,0,0,1,1-1H9a1,1,0,0,0,1-1V17a1,1,0,0,1,1-1h.172a2,2,0,0,0,1.414-.586L13.4,14.6a6.5,6.5,0,1,0-4-4Z" stroke={color} stroke-linecap="round" stroke-linejoin="round" stroke-width="2" fill="none"/>
-                    <circle cx="16.5" cy="7.5" r="0.5" stroke={color} stroke-linecap="round" stroke-linejoin="round" stroke-width="2"/>
+                    <path d="M2.586,17.414A2,2,0,0,0,2,18.828V21a1,1,0,0,0,1,1H6a1,1,0,0,0,1-1V20a1,1,0,0,1,1-1H9a1,1,0,0,0,1-1V17a1,1,0,0,1,1-1h.172a2,2,0,0,0,1.414-.586L13.4,14.6a6.5,6.5,0,1,0-4-4Z" stroke={color} strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" fill="none"/>
+                    <circle cx="16.5" cy="7.5" r="0.5" stroke={color} strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"/>
                 </g>
             )}
             {type == "vote" && (
                 <g>
-                    <circle cx="25" cy="25" r="9" fill="none" stroke={color} stroke-width="2"/>
-                    <path d="M35,15,15,35" fill="none" stroke={color} stroke-linecap="round" stroke-linejoin="round" stroke-width="2"/>
-                    <path d="M15,15,35,35" fill="none" stroke={color} stroke-linecap="round" stroke-linejoin="round" stroke-width="2"/>
+                    <circle cx="25" cy="25" r="9" fill="none" stroke={color} strokeWidth="2"/>
+                    <path d="M35,15,15,35" fill="none" stroke={color} strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"/>
+                    <path d="M15,15,35,35" fill="none" stroke={color} strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"/>
                 </g>
             )}
         </svg>
