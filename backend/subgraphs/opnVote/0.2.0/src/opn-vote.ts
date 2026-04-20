4433e9639a81de9b55019ba1b80ad97e159a32de
shadmau <17837186+shadmau@users.noreply.github.com>
2026-04-20 07:07:11 +0200

    Revert "feat(subgraph): Add AuthorizationProvider entity, populated via rpc" RPC approach did not work in production.
    This reverts commit 838eb60cc8f8c34f10a7bafce3b573df03f8070e.

--- backend/subgraphs/opnVote/0.2.0/src/opn-vote.ts
+++ backend/subgraphs/opnVote/0.2.0/src/opn-vote.ts
@@ -12,10 +12,8 @@ import {
   VoterRegistered as VoterRegisteredEvent,
   VotersAuthorized as VotersAuthorizedEvent,
   VotersRegistered as VotersRegisteredEvent,
-  OpnVote,
 } from '../generated/OpnVote/OpnVote'
 import {
-  AuthorizationProvider,
   Election,
   ElectionCanceled,
   ElectionRegisterPublicKeySet,
@@ -30,28 +28,9 @@ import {
   VotersAuthorized,
   VotersRegistered,
 } from '../generated/schema'
-import { Address, BigInt, ipfs, log } from '@graphprotocol/graph-ts'
+import { ipfs, log } from '@graphprotocol/graph-ts'
 
-function loadOrCreateAp(apId: i32, contractAddress: Address): AuthorizationProvider | null {
-  let id = apId.toString()
-  let ap = AuthorizationProvider.load(id)
-  if (ap != null) return ap
-
-  let contract = OpnVote.bind(contractAddress)
-  let result = contract.try_aps(apId)
-  if (result.reverted) {
-    log.error('contract.aps({}) reverted', [id])
-    return null
-  }
-
-  ap = new AuthorizationProvider(id)
-  ap.apId = apId
-  ap.owner = result.value.getOwner()
-  ap.apName = result.value.getApName()
-  ap.apUri = result.value.getApUri()
-  ap.save()
-  return ap
-}
+import { BigInt } from '@graphprotocol/graph-ts'
 
 export function handleElectionCanceled(event: ElectionCanceledEvent): void {
   let entity = new ElectionCanceled(event.transaction.hash.concatI32(event.logIndex.toI32()))
@@ -91,9 +70,6 @@ export function handleElectionCreated(event: ElectionCreatedEvent): void {
   } else {
     entity.descriptionBlob = ''
   }
-
-  let ap = loadOrCreateAp(event.params.authProviderId, event.address)
-  if (ap != null) entity.ap = ap.id
 
   entity.save()
 }
@@ -201,15 +177,6 @@ export function handleElectionUpdated(event: ElectionUpdatedEvent): void {
   entity.transactionHash = event.transaction.hash
 
   entity.save()
-
-  let electionEntity = Election.load(event.params.electionId.toString())
-  if (electionEntity != null) {
-    let ap = loadOrCreateAp(event.params.authProviderId, event.address)
-    if (ap != null) {
-      electionEntity.ap = ap.id
-      electionEntity.save()
-    }
-  }
 }
 
 export function handleOwnershipTransferred(event: OwnershipTransferredEvent): void {
