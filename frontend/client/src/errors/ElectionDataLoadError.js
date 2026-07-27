000addcdfbe7fa1b4a724ae1d2a8de5d5e3397c1
svalyi-op <129736832+svalyi-op@users.noreply.github.com>
2026-07-27 11:16:25 +0200

    better wording

--- frontend/client/src/errors/ElectionDataLoadError.js
+++ frontend/client/src/errors/ElectionDataLoadError.js
@@ -1,5 +1,5 @@
 export default class ElectionDataLoadError {
-    text = 'common.apploadfailed';
-    title = 'common.apploadfailed';
+    text = 'dataload.error.text';
+    title = 'dataload.error.headline';
     button = 'common.back';
 }
