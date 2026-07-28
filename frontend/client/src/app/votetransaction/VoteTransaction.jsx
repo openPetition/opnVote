b67e16c56d9b78074a631477a68b18b2a4d21fe7
svalyi-op <129736832+svalyi-op@users.noreply.github.com>
2026-07-28 14:22:00 +0200

    change link

--- frontend/client/src/app/votetransaction/VoteTransaction.jsx
+++ frontend/client/src/app/votetransaction/VoteTransaction.jsx
@@ -104,7 +104,7 @@ export default function VoteTransaction() {
 
     const BlockchainLinkText = (props) => {
         const { transactionHash } = props;
-        const shortLink = `https://gnosisscan.io/tx/${transactionHash}`;
+        const shortLink = `https://gnosis.blockscout.com/tx/${transactionHash}`;
         return (
             <Link
                 target="_blank"
