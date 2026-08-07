ab3bcdb6102d28d8d9ee13b0226ce548c5b028ab
svalyi-op <129736832+svalyi-op@users.noreply.github.com>
2026-08-07 11:23:07 +0200

    revert debug change

--- frontend/client/src/app/pollingstation/Pollingstation.jsx
+++ frontend/client/src/app/pollingstation/Pollingstation.jsx
@@ -131,7 +131,11 @@ export default function Pollingstation() {
 
     useEffect(() => {
         const currentTime = Math.floor(new Date().getTime() / 1000);
-        const state = globalConst.electionState.PLANNED;
+        const state = Number(currentTime) < Number(election.votingStartTime)
+            ? globalConst.electionState.PLANNED
+            : Number(currentTime) < Number(election.votingEndTime)
+                ? globalConst.electionState.ONGOING
+                : globalConst.electionState.FINISHED;
         setElectionState(state);
         const tempStartTime = new Date(Number(voting.election.votingStartTime) * 1000);
         setStartDate(tempStartTime);
